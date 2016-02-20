1c1f81e6487d7e06f5765dac4df23f472b418c51
shao02 <shao02@users.noreply.github.com>
2016-02-19 23:28:49 -0500

    Added files via upload

--- Functional_programming_in_scala/src/functional_programming/dataStructures/ListSample.scala
+++ Functional_programming_in_scala/src/functional_programming/dataStructures/ListSample.scala
@@ -1,6 +1,11 @@
 
 //sealed trait
 sealed trait List[+A]
+sealed trait Tree[+A]
+
+case object Nil extends Tree[Nothing]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left:Tree[A],right: Tree[A]) extends Tree[A]
 
 case object None extends List[Nothing]
 case class Cons[+A](head:A,tail:List[A]) extends List[A]{
@@ -8,6 +13,27 @@ case class Cons[+A](head:A,tail:List[A]) extends List[A]{
      head.toString()+","+tail.toString()
    }
 }
+
+
+object Tree {
+  def size[A](tr:Tree[A]):Int = tr match {
+    case Nil => 0
+    case Leaf(a) => 1
+    case Branch(l,r) => size(l) + size(r)
+  }
+  
+  def maxTree(tr:Tree[Int]):Int = tr match {
+    case Nil => -1
+    case Leaf(a) => a
+    case Branch(l,r) => maxTree(l) max maxTree(r)
+  }
+  
+  def depth[A](tr:Tree[A]):Int = tr match {
+    case Nil => 0
+    case Branch(x,y) => 1 + (depth(x) max depth(y))
+  }
+}
+
 
 object List {
   def sum(ints:List[Int]): Int = ints match {
@@ -33,6 +59,21 @@ object List {
     case _ => ints
   }
   
+  def setHead[A](ints:List[A],newhead:A)=ints match{
+    case None => List(newhead)
+    case Cons(h,t) => append(List(newhead),t)
+  }
+  
+  def append[A](first:List[A],second:List[A]):List[A] = first match {
+    case None => second
+    case Cons(h,t) => Cons(h,append(t,second))
+  }
+  
+  def foldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B = l match {
+    case None => z
+    case Cons(a,b) => foldRight(b,f(a,z))(f)
+  }
+  
   def apply[A](as: A*): List[A] =
     if(as.isEmpty) None
     else Cons(as.head,apply(as.tail : _*))
@@ -41,7 +82,7 @@ object List {
 
 object killer2 {
   def main(args: Array[String]): Unit = {
-    val ab = List(2,4,5,9,6)
+    val ab = List(2,4,5,9,4)
     val ab1 = List(2)
     val none = List()
   	println("total "+List.sum(ab))
@@ -69,5 +110,12 @@ object killer2 {
     
     println("Drop if list none:"+List.dropIfTrue(none,(a:Int)=>a>1))
     println("Drop if list ab:"+List.dropIfTrue(ab,(a:Int)=>a<5))
+    
+    println("Set head ab1:"+List.setHead(ab1, 3))
+    println("Set head ab:"+List.setHead(ab, 3))
+    
+    //println("Test foldRight ab1:"+List.foldRight(ab1, 3)((a:Int,b:Int)=>a+b))
+    println("Test foldRight ab1:"+List.foldRight(ab1, 3)(_+_))
+    println("Test foldRight ab1:"+List.foldRight(ab1, 3)(_*_))
  }
 }
