d782027b4d0adf87146853411a8c47fa0790f8af
shao02 <shao02@users.noreply.github.com>
2016-02-28 23:36:19 -0500

    Added files via upload

--- Functional_programming_in_scala/src/functional_programming/ExceptionHandlerV2.scala
+++ Functional_programming_in_scala/src/functional_programming/ExceptionHandlerV2.scala
@@ -15,9 +15,45 @@ object ExceptionHandlerV2 {
     else Some(xs.sum / xs.length)
   }
   
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] = {
+    None
+  }
+  
+  // keep that in mind that, flatMap takes a function that converse type A to Option[C]
+  // map takes a function that converse type B to 
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    // fun that converse a to c.
+    a flatMap( aa => b map (bb => f(aa,bb)))
+    None
+  }
+  
+  import java.util.regex._
+  def pattern(s:String):Option[Pattern] = {
+    try{
+      Some(Pattern.compile(s))
+    }catch{
+      case e: PatternSyntaxException => None
+    } 
+  }
+  
+  def mkMatcher_1(pat: String): Option[String => Boolean] ={
+    for {
+      k <- pattern(pat)
+    } yield ((s:String) => k.matcher(s).matches())
+  }
+  
+  def doesMatch(pat: String, s: String): Option[Boolean] ={
+    for {
+      y <- mkMatcher_1(pat)
+    } yield y(s)
+  }
+  
+  def mkMatcher(pat: String): Option[String => Boolean] ={
+    pattern(pat) map (p => (s:String) => p.matcher(s).matches())
+  }
   
   ///!!!! return a function expects to take Option[A] and return Option[B]... _ is Option[A]
-  def lift[A,B](f:A=>B):Option[A] =>Option[B] = _ map f
+  def lift[A,B](f:A=>B):Option[A] => Option[B] = _ map f
   
   //flatMap[B](f:A=> Option[B]):Option[B]
   
@@ -27,4 +63,7 @@ object ExceptionHandlerV2 {
   
   
   
+  sealed trait Either[+E,+A]
+  case class Left[+E](value:E) extends Either[E,Nothing]
+  case class Right[+A](value:A) extends Either[Nothing,A]
 }
